58607c0fb2d03211b4f3718d0931cb49339036f5
Denis Luchnikov <lasvegas3600@gmail.com>
2020-09-01 17:34:12 +0300

    Add main and news api

---
+++ src/javascript/api/NewsApi.js
@@ -0,0 +1,25 @@
+export default class ApiNews {
+  constructor(config, dates) {
+    this.config = config;
+    this.url = config.url;
+    this.endpoint = config.endpoint;
+    this.pageSize = config.pageSize;
+    this.apiKey = config.apiKey;
+    this.dates = dates;
+  }
+
+  getArticles(keyWord) {
+    sessionStorage.clear('articles');
+
+    return fetch(
+      `${this.url}${this.endpoint}?q=${keyWord}&from=${this.dates.from}&to=${this.dates.to}&pageSize=${this.pageSize}&apiKey=${this.apiKey}`,
+    )
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        return Promise.reject(res.status);
+      })
+      .catch((err) => console.log(err));
+  }
+}
