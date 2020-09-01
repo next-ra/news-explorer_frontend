8db50650af71df8605e4b4178a1cfbc7c5cac3dd
Denis Luchnikov <lasvegas3600@gmail.com>
2020-09-01 17:26:39 +0300

    Add initial files

---
+++ src/javascript/constants/main-api-config.js
@@ -0,0 +1,8 @@
+const MAIN_API_CONFIG = {
+  url: 'http://localhost:3000',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+};
+
+export default MAIN_API_CONFIG;
---
+++ src/javascript/constants/news-api-config.js
@@ -0,0 +1,8 @@
+const API_NEWS_CONFIG = {
+  url: 'https://newsapi.org/v2/',
+  endpoint: 'everything',
+  pageSize: '100',
+  apiKey: 'dcba1d73d85349c38b0ee02bcfff91d3',
+};
+
+export default API_NEWS_CONFIG;
